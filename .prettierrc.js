1934de5fede2302bed38328d39fb2fd71ae1c71d
Matheus <matheuscardhosoo@gmail.com>
2026-02-17 19:04:23 -0300

    devops: configure prettier

---
+++ .prettierrc.js
@@ -0,0 +1,18 @@
+{
+  "printWidth": 80,
+  "proseWrap": "always",
+  "singleQuote": true,
+  "trailingComma": "all",
+  "tabWidth": 2,
+  "semi": true,
+  "overrides": [
+    {
+      "files": "*.md",
+      "options": {
+        "parser": "markdown",
+        "proseWrap": "preserve",
+        "embeddedLanguageFormatting": "auto"
+      }
+    }
+  ]
+}
